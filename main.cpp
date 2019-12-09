4b3b895f73802ef0fed02d2f663483657934ce02
Jakub Skrajny <jk299999@students.mimuw.edu.pl>
2019-12-09 22:12:58 +0100

    ta jeee

--- main.cpp
+++ main.cpp
@@ -29,6 +29,10 @@ public:
         start = l;
         stop = p;
     }
+    span(){
+        start = -1;
+        stop = -1;
+    }
 } ;
 
 long long fRR(span *s){
@@ -37,6 +41,15 @@ long long fRR(span *s){
             s->l->RR*s->r->RR+
             s->l->RR*s->r->GR+
             s->l->RG*s->r->RR
+            )%CONST;
+}
+
+long long fRR(span *l, span *r){
+    return (l->RR+
+            r->RR+
+            l->RR*r->RR+
+            l->RR*r->GR+
+            l->RG*r->RR
             )%CONST;
 }
 
@@ -49,12 +62,30 @@ long long fGG(span *s){
             )%CONST;
 }
 
+long long fGG(span *l, span *r){
+    return (l->GG+
+            r->GG+
+            l->GG*r->RG+
+            l->GR*r->GG+
+            l->GR*r->RG
+            )%CONST;
+}
+
 long long fGR(span *s){
     return (s->l->GR+
             s->r->GR+
             s->l->GG*s->r->RR+
             s->l->GR*s->r->GR+
             s->l->GR*s->r->RR
+           )%CONST;
+}
+
+long long fGR(span *l, span *r){
+    return (l->GR+
+            r->GR+
+            l->GG*r->RR+
+            l->GR*r->GR+
+            l->GR*r->RR
            )%CONST;
 }
 
@@ -67,6 +98,15 @@ long long fRG(span *s){
            )%CONST;
 }
 
+long long fRG(span *l, span *r){
+    return (l->RG+
+            r->RG+
+            l->RR*r->GG+
+            l->RG*r->RG+
+            l->RR*r->RG
+           )%CONST;
+}
+
 void count(span *s){
     s->RR = fRR(s);
     s->GG = fGG(s);
@@ -74,8 +114,15 @@ void count(span *s){
     s->GR = fGR(s);
 }
 
+void count(span *s, span *l, span *r){
+    s->RR = fRR(l, r);
+    s->GG = fGG(l, r);
+    s->RG = fRG(l, r);
+    s->GR = fGR(l, r);
+}
+
 void update(span *s) {
-    if(s == NULL || !s->ready) return;
+    if(s == NULL || s->ready) return;
     span *u = s->l;
     s->l = s->r;
     s->r = u;
@@ -110,8 +157,10 @@ void build(int stage, span *s){
 
 // find biggest span that starts with wanted
 span* find(int l, int p, span *s){
-    int c = (s->start+s->stop)/2;
-    while(s->start != l && s->stop > p){
+    int c;
+    while(s->start != l || s->stop > p){
+	c = (s->start+s->stop)/2;
+	cout << s->start << " " << s->stop << endl; 
         if(l == s->start) {
             s = s->l;
         } else {
@@ -123,22 +172,36 @@ span* find(int l, int p, span *s){
         }
         update(s);
     }
+	cout << endl;
     return s;
 }
 
 long long query(int l, int p, span *tree){
-    int sum = 0;
-    while(l < p){
+    vector<span> v;
+    while(l <= p){
         span *s = find(l, p, tree);
         while(s->stop > p){
             s = s->l;
             update(s);
             assert(s != NULL);
+	    //cout << s->start << " " << s->stop << endl; 
+	    //cout << s->RR << " " << s->GG << " " << s->RG << " " << s->GR << endl;
         }
-        sum += s->sum();
+	v.push_back(*s);
         l = s->stop+1;
     }
-    return sum;
+    vector<span> w;
+    while(v.size() > 1){
+	int i = 0;
+	while(i<v.size()-1){	
+	    w.push_back(span());
+	    count(&w[i/2], &v[i], &v[i+1]);
+ 	    i += 2;
+	}
+	if(i == v.size()-1) w.push_back(v[i]);
+	v = move(w);
+    }
+    return v[0].sum();
 }
 
 void rotate(int l, int p){
@@ -157,8 +220,9 @@ int main() {
         char c;
         int start, finish;
         cin >> c >> start >> finish;
+	cout << "wczytane" << endl;
         if (c == '?') {
-            cout << query(start, finish, tree) << endl;
+           cout << query(start, finish, tree) << endl;
         } else {
             rotate(start, finish);
         }
